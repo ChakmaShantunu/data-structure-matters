9d5997b77055f44d3f0efc03b243b26e0e806227
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-04 22:30:27 +0600

    object are statefull with a simple counter example done

--- stateful.js
+++ stateful.js
@@ -1,1 +1,17 @@
 //object are stateful
+
+const counter = {
+    count: 0,
+
+    add(amount) {
+        this.count = this.count + amount
+    },
+
+    print() {
+        console.log(this.count)
+    }
+};
+
+counter.add(2);
+counter.add(3);
+counter.print();
